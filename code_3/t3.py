091bc6fd8988055ca35bea990e62131c0e81bc24
Arsen0105 <118631903+Arsen0105@users.noreply.github.com>
2024-03-05 22:57:28 +0700

    Update t3.py

--- code_3/t3.py
+++ code_3/t3.py
@@ -5,7 +5,7 @@ def t():
 	for i in range(a, b + 1):
 		if n - i > 0:
 			if not d[n - i]:
-				print(i)
+				print(i + flag)
 
 def check(n):
 	global d
@@ -20,6 +20,10 @@ def check(n):
 n, a, b = 23, 1, 3
 # False - loss, True - win
 d = {i: False for i in range(1, n)}
+
+# True - The one who takes the last match wins
+# False - The one who takes the last match loses
+flag = True
 
 t()
 print(d)
